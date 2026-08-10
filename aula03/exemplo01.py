b1f0b83a2647c13c601e0fe36c719dbe26cf25d2
AmandaSenac123 <almeidamanda@hotmail.com>
2026-08-10 20:45:11 -0300

    implementa algoritimo ex01

--- aula03/exemplo01.py
+++ aula03/exemplo01.py
@@ -1,2 +1,16 @@
 # GitHub
 print('Github - Aula 03')
+
+#Exemplo01 - Veiculo 10 km/1
+CONSUMO = 10 # Constante
+distancia1 = float(input('Informe a distância: '))
+distancia2 = float(input('Informe a outra distância: '))
+
+# Processamento
+distancia_total = distancia1 + distancia2
+combustivel = distancia_total / CONSUMO
+
+# Saída
+
+print (f'Distância percorrida {distancia_total}')
+print (f'Consumo de {combustivel}')
